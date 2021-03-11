ddc1d135fa392209f5e62f6645ca707eddc13a2b
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-03-12 02:30:44 +0300

    PSR-4 autoloader

---
+++ autoloader.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * The PSR-4 autoloader `Sixa` project implementation.
+ * After registering this autoload function with SPL, the following line
+ * would cause the function to attempt to load the classes from different directories.
+ *
+ * @link       https://sixa.ch
+ * @author     Mahdi Yazdani
+ * @since      1.0.0
+ *
+ * @package    sixa-snippets
+ */
+
+namespace SixaSnippets;
+
+spl_autoload_register(
+	function( string $filename ): void { // phpcs:ignore PHPCompatibility.FunctionDeclarations.NewReturnTypeDeclarations.voidFound
+		// First, separate the components of the incoming file.
+		$file_path = explode( '\\', $filename );
+
+		/**
+		 * - The first index will always be the namespace since it's part of the plugin.
+		 * - All but the last index will be the path to the file.
+		 * - The final index will be the filename. If it doesn't begin with 'I' then it's a class.
+		 */
+
+		// Get the last index of the array. This is the class we're loading.
+		$file_name = '';
+		if ( isset( $file_path[ count( $file_path ) - 1 ] ) ) {
+
+			$file_name = strtolower(
+				$file_path[ count( $file_path ) - 1 ]
+			);
+
+			$file_name       = str_ireplace( '_', '-', $file_name );
+			$file_name_parts = explode( '-', $file_name );
+
+			// Interface support: handle both Interface_Foo or Foo_Interface.
+			$index = array_search( 'interface', $file_name_parts, true );
+
+			if ( false !== $index ) {
+				// Remove the 'interface' part.
+				unset( $file_name_parts[ $index ] );
+				// Rebuild the file name.
+				$file_name = implode( '-', $file_name_parts );
+				$file_name = sprintf( 'interface-%s.php', $file_name );
+			} else {
+				$file_name = sprintf( 'class-%s.php', $file_name );
+			}
+		}
+
+		/**
+		 * Find the fully qualified path to the class file by iterating through the $file_path array.
+		 * We ignore the first index since it's always the top-level package. The last index is always
+		 * the file so we append that at the end.
+		 */
+		$fully_qualified_path = trailingslashit( plugin_dir_path( __FILE__ ) );
+		$count_file_path      = count( $file_path );
+
+		for ( $i = 1; $i < $count_file_path - 1; $i++ ) {
+			$dir                   = strtolower( $file_path[ $i ] );
+			$fully_qualified_path .= trailingslashit( $dir );
+		}
+
+		$fully_qualified_path .= $file_name;
+
+		// Now include the file.
+		// Checks if a file is readable.
+		if ( is_readable( $fully_qualified_path ) ) {
+			require_once $fully_qualified_path; // Do not stop PHP from executing the rest of the script.
+		}
+	}
+);

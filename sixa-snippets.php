ff103a7b6db2217ffa65ccde0bbd8c1b29532def
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-03-12 02:39:36 +0300

    The plugin bootstrap file

---
+++ sixa-snippets.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The plugin bootstrap file.
+ *
+ * @link              https://sixa.ch
+ * @author            Mahdi Yazdani <info@sixa.ch>
+ * @since             1.0.0
+ * @package           sixa-snippets
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Sixa Snippets
+ * Plugin URI:        https://sixa.ch
+ * Description:       A plugin containing factory classes or methods for the Sixa projects.
+ * Version:           1.0.0
+ * Requires at least: WordPress 5.3
+ * Requires PHP:      7.2
+ * Author:            sixa AG
+ * Author URI:        https://sixa.ch
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       @@textdomain
+ * Domain Path:       /languages
+ */
+
+namespace SixaSnippets;
+
+/**
+ * Loads the PSR-4 autoloader implementation.
+ *
+ * @since    1.0.0
+ * @return   void
+ */
+require_once sprintf( '%s/autoloader.php', untrailingslashit( plugin_dir_path( __FILE__ ) ) );

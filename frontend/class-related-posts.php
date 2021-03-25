59dc20a95bc0b8d2b2e1192c5e2dbcae2388f33f
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-03-26 01:11:38 +0300

    Added `Categories` support
    Adjusted CSS class names used in generating output HTML markup

--- frontend/class-related-posts.php
+++ frontend/class-related-posts.php
@@ -25,6 +25,14 @@ if ( ! class_exists( 'Related_Posts' ) ) :
 	class Related_Posts {
 
 		/**
+		 * CSS class name.
+		 *
+		 * @access   private
+		 * @var      string    $class    CSS class name used for this component.
+		 */
+		private static $class = 'sixa-related-posts';
+
+		/**
 		 * Run this class and output generated posts on the page.
 		 *
 		 * @since    1.0.0
@@ -34,23 +42,25 @@ if ( ! class_exists( 'Related_Posts' ) ) :
 		 */
 		public static function run( $args = array(), $echo = true ) {
 			/* translators: 1: Open `div` tag, 2: Close H3 tag. */
-			$return = sprintf( esc_html_x( '%1$sRelated Posts%2$s', 'related posts', '@@textdomain' ), '<div class="sixa-related-posts"><h3>', '</h3>' );
+			$return = sprintf( esc_html_x( '%1$sRelated Posts%2$s', 'related posts', '@@textdomain' ), sprintf( '<div class="%s"><h3>', sanitize_html_class( self::$class ) ), '</h3>' );
 
 			// Bail early, in case the query is NOT for an existing single post of any post type.
 			if ( is_singular() ) {
 				$args = wp_parse_args(
 					$args,
 					array(
-						'show_date'   => 1,
-						'show_author' => 1,
-						'show_thumb'  => 1,
+						'show_date'       => 1,
+						'show_author'     => 1,
+						'show_thumb'      => 1,
+						'show_categories' => 1,
 					)
 				);
 
-				$show_date   = ( 1 === intval( $args['show_date'] ) || 'true' === $args['show_date'] ) ? true : false;
-				$show_author = ( 1 === intval( $args['show_author'] ) || 'true' === $args['show_author'] ) ? true : false;
-				$show_thumb  = ( 1 === intval( $args['show_thumb'] ) || 'true' === $args['show_thumb'] ) ? true : false;
-				$get_posts   = self::generate( $args );
+				$show_date       = ( 1 === intval( $args['show_date'] ) || 'true' === $args['show_date'] ) ? true : false;
+				$show_author     = ( 1 === intval( $args['show_author'] ) || 'true' === $args['show_author'] ) ? true : false;
+				$show_thumb      = ( 1 === intval( $args['show_thumb'] ) || 'true' === $args['show_thumb'] ) ? true : false;
+				$show_categories = ( 1 === intval( $args['show_categories'] ) || 'true' === $args['show_categories'] ) ? true : false;
+				$get_posts       = self::generate( $args );
 
 				// Bail early, if the query has no posts to loop over.
 				if ( is_array( $get_posts ) && ! empty( $get_posts ) ) {
@@ -61,21 +71,26 @@ if ( ! class_exists( 'Related_Posts' ) ) :
 
 						// Thumbnail.
 						if ( ! ! $show_thumb && has_post_thumbnail( $post ) ) {
-							$return .= sprintf( '<figure class="entry-thumbnail"><a href="%s">%s</a></figure>', esc_url( get_permalink( $post ) ), get_the_post_thumbnail( $post ) );
+							$return .= sprintf( '<figure class="%s__thumbnail"><a href="%s">%s</a></figure>', sanitize_html_class( self::$class ), esc_url( get_permalink( $post ) ), get_the_post_thumbnail( $post ) );
 						}
 
 						// Title.
-						$return .= sprintf( '<a href="%s" class="entry-title" rel="bookmark">%s</a>', esc_url( get_permalink( $post ) ), wp_kses_post( get_the_title( $post ) ) );
+						$return .= sprintf( '<a href="%s" class="%s__title" rel="bookmark">%s</a>', sanitize_html_class( self::$class ), esc_url( get_permalink( $post ) ), wp_kses_post( get_the_title( $post ) ) );
 
 						// Author.
 						if ( ! ! $show_author ) {
 							/* translators: 1: Open span tag, 2: Author anchor tag, 3: Close span tag. */
-							$return .= sprintf( _x( '%1$sby %2$s%3$s', 'related posts', '@@textdomain' ), '<span class="post-author">', sprintf( '<a href="%s" class="url fn" rel="author">%s</a>', esc_url( get_author_posts_url( get_the_author_meta( 'ID', $post->post_author ) ) ), get_the_author_meta( 'display_name', $post->post_author ) ), '</span>' );
+							$return .= sprintf( _x( '%1$sby %2$s%3$s', 'related posts', '@@textdomain' ), sprintf( '<span class="%s__author">', sanitize_html_class( self::$class ) ), sprintf( '<a href="%s" class="url fn" rel="author">%s</a>', esc_url( get_author_posts_url( get_the_author_meta( 'ID', $post->post_author ) ) ), get_the_author_meta( 'display_name', $post->post_author ) ), '</span>' );
 						}
 
 						// Date.
 						if ( ! ! $show_date ) {
-							$return .= sprintf( '<a href="%s" rel="bookmark"><time class="entry-date published" datetime="%s">%s</time></a>', esc_url( get_permalink( $post ) ), esc_attr( get_the_date( 'c' ) ), esc_html( get_the_date() ) );
+							$return .= sprintf( '<time class="%s__date published" datetime="%s">%s</time>', sanitize_html_class( self::$class ), esc_attr( get_the_date( 'c' ) ), esc_html( get_the_date() ) );
+						}
+
+						// Categories.
+						if ( ! ! $show_categories && is_single() ) {
+							$return .= sprintf( '<div class="%s__categories">%s</div>', sanitize_html_class( self::$class ), get_the_category_list() );
 						}
 
 						$return .= '</li>';

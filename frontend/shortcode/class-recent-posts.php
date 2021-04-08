9d8353fd9228615c671f066b248702e6a264fa48
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-04-08 14:58:26 +0300

    Added new shortcode attributes support

--- frontend/shortcode/class-recent-posts.php
+++ frontend/shortcode/class-recent-posts.php
@@ -35,69 +35,155 @@ if ( ! class_exists( 'Recent_Posts' ) ) :
 		public static function run( $atts ) {
 			$atts = shortcode_atts(
 				array(
-					'categories'  => '',
-					'number'      => 4,
-					'show_date'   => 0,
-					'show_author' => 0,
-					'show_thumb'  => 0,
-					'order'       => 'desc',
-					'orderby'     => 'date',
+					'class'                      => '',
+					'categories'                 => '',
+					'selected_author'            => '',
+					'number'                     => 5,
+					'display_post_content'       => 0,
+					'display_post_content_radio' => 'excerpt',
+					'excerpt_length'             => 55,
+					'display_author'             => 0,
+					'display_post_date'          => 0,
+					'columns'                    => 3,
+					'order'                      => 'desc',
+					'order_by'                   => 'date',
+					'display_featured_image'     => 0,
+					'featured_image_size_slug'   => 'thumbnail',
+					'featured_image_size_width'  => '',
+					'featured_image_size_height' => '',
 				),
 				$atts
 			);
 
-			$return      = '';
-			$categories  = ! empty( $atts['categories'] ) ? explode( ',', $atts['categories'] ) : array();
-			$number      = intval( $atts['number'] );
-			$order       = esc_html( $atts['order'] );
-			$orderby     = esc_html( $atts['orderby'] );
-			$show_date   = ( 1 === intval( $atts['show_date'] ) || 'true' === $atts['show_date'] ) ? true : false;
-			$show_author = ( 1 === intval( $atts['show_author'] ) || 'true' === $atts['show_author'] ) ? true : false;
-			$show_thumb  = ( 1 === intval( $atts['show_thumb'] ) || 'true' === $atts['show_thumb'] ) ? true : false;
-			$get_posts   = get_posts(
-				apply_filters(
-					'sixa_recent_posts_shortcode_args',
-					array(
-						'order'            => $order,
-						'orderby'          => $orderby,
-						'category__in'     => $categories,
-						'numberposts'      => $number,
-						'post_status'      => 'publish',
-						'suppress_filters' => false,
-					)
+			// Update the maximum number of words in a post excerpt.
+			$excerpt_length = $atts['excerpt_length'];
+			add_filter(
+				'excerpt_length',
+				function() use ( $excerpt_length ) {
+					return $excerpt_length;
+				},
+				20
+			);
+
+			$query_args = apply_filters(
+				'sixa_recent_posts_shortcode_args',
+				array(
+					'order'            => $atts['order'],
+					'orderby'          => $atts['order_by'],
+					'posts_per_page'   => $atts['number'],
+					'post_status'      => 'publish',
+					'suppress_filters' => false,
 				)
 			);
 
+			if ( ! empty( $atts['categories'] ) ) {
+				$query_args['category__in'] = array_column( $atts['categories'], 'id' );
+			}
+			if ( ! empty( $atts['selected_author'] ) ) {
+				$query_args['author'] = $atts['selected_author'];
+			}
+
+			$return    = '';
+			$class     = empty( $atts['class'] ) ? sprintf( 'wp-block-sixa-recent-posts' ) : '';
+			$get_posts = get_posts( $query_args );
+
 			// Bail early, if the query has no posts to loop over.
 			if ( is_array( $get_posts ) && ! empty( $get_posts ) ) {
-				$return = '<ul>';
+				$return = '';
 
 				foreach ( $get_posts as $post ) {
-					$return .= sprintf( '<li class="%s">', implode( ' ', get_post_class( '', $post ) ) );
+					$return .= sprintf( '<div class="%s">', implode( ' ', get_post_class( '', $post ) ) );
 
 					// Thumbnail.
-					if ( ! ! $show_thumb && has_post_thumbnail( $post ) ) {
-						$return .= sprintf( '<figure class="entry-thumbnail"><a href="%s">%s</a></figure>', esc_url( get_permalink( $post ) ), get_the_post_thumbnail( $post ) );
+					if ( $atts['display_featured_image'] && has_post_thumbnail( $post ) ) {
+						$image_style = '';
+						if ( isset( $atts['featured_image_size_width'] ) ) {
+							$image_style .= sprintf( 'max-width:%spx;', $atts['featured_image_size_width'] );
+						}
+						if ( isset( $atts['featured_image_size_height'] ) ) {
+							$image_style .= sprintf( 'max-height:%spx;', $atts['featured_image_size_height'] );
+						}
+
+						$featured_image = sprintf(
+							'<a href="%1$s">%2$s</a>',
+							esc_url( get_permalink( $post ) ),
+							get_the_post_thumbnail(
+								$post,
+								$atts['featured_image_size_slug'],
+								array(
+									'style' => $image_style,
+								)
+							)
+						);
+						$return        .= sprintf(
+							'<figure class="%1$s">%2$s</figure>',
+							sprintf( '%s__post-thumbnail', sanitize_html_class( $class ) ),
+							$featured_image
+						);
 					}
 
 					// Title.
-					$return .= sprintf( '<a href="%s" class="entry-title" rel="bookmark">%s</a>', esc_url( get_permalink( $post ) ), wp_kses_post( get_the_title( $post ) ) );
+					$title = get_the_title( $post );
+					if ( ! $title ) {
+						$title = __( '(no title)', '@@textdomain' );
+					}
+					$return .= sprintf( '<a href="%s" class="%s" rel="bookmark">%s</a>', esc_url( get_permalink( $post ) ), sprintf( '%s__post-title', sanitize_html_class( $class ) ), wp_kses_post( $title ) );
 
 					// Author.
-					if ( ! ! $show_author ) {
-						/* translators: 1: Open span tag, 2: Author anchor tag, 3: Close span tag. */
-						$return .= sprintf( _x( '%1$sby %2$s%3$s', 'recent posts shortcode', '@@textdomain' ), '<span class="post-author">', sprintf( '<a href="%s" class="url fn" rel="author">%s</a>', esc_url( get_author_posts_url( get_the_author_meta( 'ID', $post->post_author ) ) ), get_the_author_meta( 'display_name', $post->post_author ) ), '</span>' );
+					if ( isset( $atts['display_author'] ) && $atts['display_author'] ) {
+						$author_display_name = get_the_author_meta( 'display_name', $post->post_author );
+
+						/* translators: byline. %s: current author. */
+						$byline = sprintf( __( 'by %s', '@@textdomain' ), $author_display_name );
+
+						if ( ! empty( $author_display_name ) ) {
+							$return .= sprintf(
+								'<div class="%s">%1$s</div>',
+								sprintf( '%s__post-author', sanitize_html_class( $class ) ),
+								esc_html( $byline )
+							);
+						}
 					}
 
 					// Date.
-					if ( ! ! $show_date ) {
-						$return .= sprintf( '<a href="%s" rel="bookmark"><time class="entry-date published" datetime="%s">%s</time></a>', esc_url( get_permalink( $post ) ), esc_attr( get_the_date( 'c' ) ), esc_html( get_the_date() ) );
+					if ( isset( $atts['display_post_date'] ) && $atts['display_post_date'] ) {
+						$return .= sprintf(
+							'<time datetime="%1$s" class="%2$s">%3$s</time>',
+							sprintf( '%s__post-date', sanitize_html_class( $class ) ),
+							esc_attr( get_the_date( 'c', $post ) ),
+							esc_html( get_the_date( '', $post ) )
+						);
 					}
 
-					$return .= '</li>';
-				}
+					// Content.
+					if ( isset( $atts['display_post_content'], $atts['display_post_content_radio'] ) && $atts['display_post_content'] ) {
+						// Excerpt.
+						if ( 'excerpt' === $atts['display_post_content_radio'] ) {
+							$trimmed_excerpt = get_the_excerpt( $post );
+							$readmore        = '';
 
-				$return .= '</ul>';
+							if ( has_excerpt( $post ) ) {
+								/* translators: 1: Open anchor link, 2: Close anchor tag.  */
+								$readmore = sprintf( esc_html__( '%1$sRead more%2$s', '@@textdomain' ), sprintf( '<a href="%s">', esc_url( get_permalink( $post ) ) ), '</a>' );
+							}
+
+							$return .= sprintf(
+								'<div class="%1$s">%2$s%3$s</div>',
+								sprintf( '%s__post-excerpt', sanitize_html_class( $class ) ),
+								get_the_excerpt( $post ),
+								$readmore,
+							);
+						} else {
+							$return .= sprintf(
+								'<div class="%1$s">%2$s</div>',
+								sprintf( '%s__post-content', sanitize_html_class( $class ) ),
+								wp_kses_post( html_entity_decode( $post->post_content, ENT_QUOTES, get_option( 'blog_charset' ) ) )
+							);
+						}
+					}
+
+					$return .= '</div>';
+				}
 			}
 
 			return apply_filters( 'sixa_recent_posts_shortcode_output', $return );

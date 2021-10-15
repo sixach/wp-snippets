c0ee0de5f3384adddb79695f4064d9d780578cb5
Markus <markus.goeckeritz@sixa.ch>
2021-10-15 16:47:18 +0200

    renamed languages file

--- sixa-snippets.php
+++ sixa-snippets.php
@@ -18,7 +18,7 @@
  * Author URI:           https://sixa.ch
  * License:              GPL-3.0+
  * License URI:          http://www.gnu.org/licenses/gpl-3.0.txt
- * Text Domain:          @@textdomain
+ * Text Domain:          sixa-snippets
  * Domain Path:          /languages
  */
 
